64dee07be6d1bf75356314cc53ac850a5432437b
GorgXD <george.luis.araujo@gmail.com>
2020-08-29 18:19:41 -0300

    projeto criado

---
+++ src/test/java/com/projeto/brotinho/BrotinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.brotinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrotinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
